553740b46cdce434c23d007f26d45e0561d6b2e0
caronfox67-lang <caronfox67@gmail.com>
2026-01-10 21:53:37 +0200

    Fix main execution and try/except error

--- bot.py
+++ bot.py
@@ -97,9 +97,10 @@ async def monitor(bot: Bot):
 async def main():
     bot = Bot(token=BOT_TOKEN)
 
-    while True:
-        try:
-            await monitor(bot)
+    try:
+        await monitor(bot)
+    except Exception as e:
+        print("ERROR:", e)
 
 
 if __name__ == "__main__":
